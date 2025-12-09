859d64bade065642b213df7d9ab5e9fca040d0b9
Manu BuddHa <ferano915@gmail.com>
2025-12-09 15:55:49 +0530

    Database Connected

--- home.py
+++ home.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template, request
 import sqlite3
 app = Flask(__name__)
+def get_userdb_connection():
+    users_db = sqlite3.connect('library_users.db')
+    user_cursor = users_db.cursor()
+    users_db.commit()
+    return users_db
+
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        name=request.form['username']
+        password=request.form['password']
+        return render_template('admin.html',name=name,password=password)
 
 app.run(debug=True)
